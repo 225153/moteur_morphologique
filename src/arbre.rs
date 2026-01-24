aef9d3c61956f18cc0f946852a02cf1e45cdcca3
ahmedtr692 <ahmedtr692@gmail.com>
2026-01-24 09:56:33 +0100

    .

--- src/arbre.rs
+++ src/arbre.rs
@@ -9,11 +9,40 @@ pub struct RacineNode {
     pub left: Option<Box<RacineNode>>,  
     pub right: Option<Box<RacineNode>>, 
 }
-pub struct Tree{Option<Box<RacineNode>>};
-
-impl Tree {
-    pub fn new(racine : char)-> Self {
 
 
+pub fn morphologic_cmp(tree_racine : [char;3], racine : [char; 3])-> i8 {
+    let mut i = 0 ; 
+    while i < 3 {
+        if tree_racine[i] == racine[i] {
+            i = i + 1 ;
+        }
+        else {
+            if tree_racine[i] > racine[i]{
+                return -1 ;
+            }
+            else {
+                return 1 ;
+            }
+        }
     }
+    return 0 ;
+}
+
+
+pub struct Tree{Option<Box<RacineNode>>};
+impl RacineNode{
+    pub fn new(racine: [char; 3]) -> Self {
+        RacineNode{
+            racine,
+            left: none ,
+            right : none,
+        }
+    }
+}
+impl Tree {
+    pub fn new() -> Self {
+        Tree { racine: None }
+    }
+    pub fn verify()
 }
